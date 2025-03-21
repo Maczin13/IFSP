28e7cfdf2a5cb3df6e01e9765126f159a8619fdd
Marcos Andrade <marcosdtsandrade13@gmail.com>
2025-03-21 16:40:24 -0300

    Add files via upload

---
+++ Treinamento/C#/EstruturaCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ 
+//Namespace
+namespace EstruturaCSharp                                      
+{
+    public class MinhaAplicacao                               
+    {
+        static void Main(string[] args)
+        //Início de bloco
+        {
+            
+            Console.WriteLine("Hello world");
+       
+        }
+     
+    }
+}
