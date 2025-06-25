d5e60dd9539362caf1742b7ea570d05356f6be5e
Marcos Andrade <marcosdtsandrade13@gmail.com>
2025-06-25 16:36:08 -0300

    Add files via upload

---
+++ ADS/PRES/Aula 10/Aula10/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, opcao;
+
+            do
+            {
+                Menu();
+                opcao = int.Parse(Console.ReadLine());
+                if (opcao != 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Informe o primeiro número");
+                    n1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe o sgundo número");
+                    n2 = int.Parse(Console.ReadLine());
+
+                    if (opcao == 1)
+                    {
+                        Console.WriteLine("Soma: {0}", Soma(n1, n2));
+                    }
+                    else if (opcao == 2)
+                    {
+                        Console.WriteLine("Subtração: {0}", Subtracao(n1, n2));
+                    }
+                    else if (opcao == 3)
+                    {
+                        Console.WriteLine("Multiplicação: {0}", Multiplicacao(n1, n2));
+                    }
+                    else
+                    {
+                        if (n2 == 0)
+                            Console.WriteLine("Erro - o divisor não pode ser zero!");
+                        else
+                            Console.WriteLine("Divisão: {0}", (double)Divisao(n1, n2));
+                    }
+                }
+            } while (opcao != 5);
+
+
+
+        }
+        private static void Menu()
+        {
+            Console.WriteLine("Menu principal");
+            Console.WriteLine("---------------");
+            Console.WriteLine("1. Somar");
+            Console.WriteLine("2. Subtrair");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("5. Sair");
+            Console.WriteLine("---------------");
+            Console.Write("Sua opção: ");
+        }
+
+        private static int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static int Subtracao(int x, int y)
+        {
+            return x - y;
+        }
+        private static int Multiplicacao(int x, int y)
+        {
+            return x * y;
+        }
+        private static int Divisao(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ ADS/PRES/Aula 11/Aula 11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static double Cubo(double x)
+            { return x * x; }
+        static bool EPar(double x)
+        {
+            if (x % 2 == 0) { return true; }
+            else { return false; }
+        }
+
+        static string ClassificarNum(double x)
+        {
+            if(x % 2 == 0) { return "Par"; }
+            else { return "Impar"; }
+        }
+        static string EhPrimo(int numero)
+        {
+            if (numero <= 1) return "nao";
+            if (numero <= 3) return "sim";
+            if (numero % 2 == 0 || numero % 3 == 0) return "nao";
+
+            for (int i = 5; i * i <= numero; i = i + 6)
+            {
+                if (numero % i == 0 || numero % (i + 2) == 0)
+                {
+                    return "Nao";
+                }
+            }
+            return "sim";
+        }
+
+    }
+}
